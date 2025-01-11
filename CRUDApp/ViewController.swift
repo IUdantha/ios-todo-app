e0cdb62922f6ca9cadd1131e71bb387c64e778a7
udanthaisuru@gmail.com <udanthaisuru@gmail.com>
2025-01-11 22:07:12 +0530

    Initial Commit

---
+++ CRUDApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CRUDApp
+//
+//  Created by Isuru Udantha on 2025-01-11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ CRUDAppTests/CRUDAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  CRUDAppTests.swift
+//  CRUDAppTests
+//
+//  Created by Isuru Udantha on 2025-01-11.
+//
+
+import Testing
+@testable import CRUDApp
+
+struct CRUDAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
