ec1b1b30b3af1df8d660dd5b492d98027e5a7d95
udanthaisuru@gmail.com <udanthaisuru@gmail.com>
2025-01-12 13:50:42 +0530

    added create/ delete functions

---
+++ CRUDApp/TaskViewController.swift
@@ -0,0 +1,34 @@
+//
+//  TaskViewController.swift
+//  CRUDApp
+//
+//  Created by Isuru Udantha on 2025-01-12.
+//
+
+import UIKit
+
+class TaskViewController: UIViewController {
+    
+    @IBOutlet var label: UILabel!
+    
+    var task: String?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        label.text = task
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Delete", style: .done, target: self, action: #selector(deleteTask))
+    }
+    
+    @objc func deleteTask(){
+        
+//        let newCount = count - 1
+//        
+//        UserDefaults().setValue(newCount, forKey: "count")
+//        UserDefaults().setValue(nil, forKey: "task_\(currentPosition)")
+        
+        
+    }
+
+}
